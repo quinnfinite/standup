e04134ab2f30576122cf0cb00c02f60099a54e26
Quinn Royston <quinn.royston@gmail.com>
2022-08-25 08:58:36 -0700

    Rename settings to config in main

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,13 @@ mod config;
 use config::build_config;
 
 fn main() {
-    let mut settings = build_config();
-    let url = settings.remove("url");
+    let mut config = build_config();
+    let url = config.remove("url");
     match url {
         Some(url) => {
             let url = String::from(url);
             open::that(url).unwrap();
         }
-        None => println!("Could not retrieve url from settings")
+        None => println!("Could not retrieve url from config")
     }
 }
