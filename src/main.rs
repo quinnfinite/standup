acd66c62b1791309a15f2e56d22a5be366e6deec
Quinn Royston <quinn.royston@gmail.com>
2022-08-24 22:43:54 -0700

    Override default settings with Custom_Settings

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,35 @@ use config::Config;
 use std::collections::HashMap;
 
 fn get_settings() -> HashMap<String, String> {
-    let settings = Config::builder()
+    let default_settings = Config::builder()
     // Add in `./Settings.toml`
     .add_source(config::File::with_name("./Default_Settings"))
-    // Add in settings from the environment (with a prefix of APP)
-    // Eg.. `APP_DEBUG=1 ./target/app` would set the `debug` key
-    .add_source(config::Environment::with_prefix("DEFAULT"))
     .build()
     .unwrap();
 
-    settings
+    let default_settings: HashMap<String, String> = default_settings
     .try_deserialize::<HashMap<String, String>>()
-    .unwrap()
+    .unwrap();
+
+    let custom_settings = Config::builder()
+    // Add in `./Custom_Settings.toml`
+    .add_source(config::File::with_name("./Custom_Settings"))
+    .build()
+    .unwrap();
+
+    let mut custom_settings: HashMap<String, String> = custom_settings
+    .try_deserialize::<HashMap<String, String>>()
+    .unwrap();
+
+    let final_settings = default_settings.into_iter().map(|(key, value)| {
+        if custom_settings.contains_key(&key) {
+            return custom_settings.remove_entry(&key).unwrap();
+        } else {
+            return (key, value);
+        }
+    }).collect::<HashMap<String, String>>();
+
+    final_settings
 }
 
 fn main() {
