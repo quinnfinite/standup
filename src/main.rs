ca0e5b19b1c5b93f60cbb9ce8172f26deb654ad9
Quinn Royston <quinn.royston@gmail.com>
2022-08-25 17:48:30 -0700

    Use command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,29 @@
 use open;
 mod config;
 use config::build_config;
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+    command: String,
+}
 
 fn main() {
+    let args = Cli::parse();
     let mut config = build_config();
-    let url = config.remove("url");
-    match url {
-        Some(url) => {
-            let url = String::from(url);
-            open::that(url).unwrap();
+    let command = args.command;
+    
+    match command.as_str() {
+        "open" => {
+            let url = config.remove("url");
+            match url {
+                Some(url) => {
+                    let url = String::from(url);
+                    open::that(url).unwrap();
+                }
+                None => println!("Could not retrieve url from config")
+            }
         }
-        None => println!("Could not retrieve url from config")
+        false_command => println!("Command {:?} not found", false_command)
     }
 }
