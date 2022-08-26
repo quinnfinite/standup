9f31d7c91e88cdc1b5b56e07d7f2357897ae7d72
Quinn Royston <quinn.royston@gmail.com>
2022-08-25 18:30:35 -0700

    Create function for notes command and going to standup
    - Replace open command with go

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use open;
 mod config;
 use config::build_config;
 use clap::Parser;
+use std::collections::HashMap;
+use chrono::prelude::*;
 
 #[derive(Parser)]
 struct Cli {
@@ -10,20 +12,39 @@ struct Cli {
 
 fn main() {
     let args = Cli::parse();
-    let mut config = build_config();
+    let config = build_config();
     let command = args.command;
     
     match command.as_str() {
-        "open" => {
-            let url = config.remove("url");
-            match url {
-                Some(url) => {
-                    let url = String::from(url);
-                    open::that(url).unwrap();
-                }
-                None => println!("Could not retrieve url from config")
-            }
+        "go" => {
+            go_to_standup(config);
+        }
+        "notes" => {
+            open_notes();
         }
         false_command => println!("Command {:?} not found", false_command)
+    }
+}
+
+fn open_notes() {
+    let local_date = Local::now().format("%Y-%m-%d").to_string();
+    let file_name = local_date + ".txt";
+    println!("file_name: {:?}", file_name);
+    std::process::Command::new("vim")
+        .arg(file_name)
+        .spawn()
+        .expect("Error: Failed to run editor")
+        .wait()
+        .expect("Error: Editor returned a non-zero status");
+}
+
+fn go_to_standup(mut config: HashMap<String, String>) {
+    let url = config.remove("url");
+    match url {
+        Some(url) => {
+            let url = String::from(url);
+            open::that(url).unwrap();
+        }
+        None => println!("Could not retrieve url from config")
     }
 }
